4275a240f8b4c4cddcb41ef97f27b0d9327d708a
VINIA6 <viniciusdeassisazevedo@hotmail.com>
2025-06-16 16:58:04 -0300

    refact: creating function teste

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,10 @@ const variavel = 'teste';
 const teste = () => {
     if (variavel === 'teste') {
         console.log('Teste de bug 1');
-    } else {
+    } else if (variavel === 'teste2') {
         console.log('Teste de bug 2');
+    } else {
+        console.log('Teste de bug 3');
     }
 }
 
